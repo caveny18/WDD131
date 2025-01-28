505ac86dcbcb24241020d4380779428309bcaa48
caveny18 <20231469@lamolina.edu.pe>
2025-01-28 18:24:24 -0500

    8

--- temples.js
+++ temples.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     // Mostrar el año actual en el footer
     document.getElementById("currentYear").textContent = new Date().getFullYear();
@@ -6,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Obtener y formatear la fecha de última modificación
     const lastModified = new Date(document.lastModified);
     const formattedDate = `${lastModified.getFullYear()}-${String(lastModified.getMonth() + 1).padStart(2, '0')}-${String(lastModified.getDate()).padStart(2, '0')} ${String(lastModified.getHours()).padStart(2, '0')}:${String(lastModified.getMinutes()).padStart(2, '0')}:${String(lastModified.getSeconds()).padStart(2, '0')}`;
-
     document.getElementById("lastModified").textContent = formattedDate;
 
-    // Evento para el botón de hamburguesa
-    document.getElementById("hamburgerButton").addEventListener("click", function() {
-        document.querySelector("nav ul").classList.toggle("visible");
+    // Evento para el botón de menú hamburguesa
+    const menuButton = document.getElementById("menuButton");
+    const menu = document.getElementById("menu");
+
+    menuButton.addEventListener("click", function() {
+        menu.classList.toggle("open");
     });
 });
